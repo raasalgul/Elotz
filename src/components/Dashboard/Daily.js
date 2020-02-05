55779ab5dc42ebcb1e7f510db69e623b419bed98
raasalgul <k.sathishrsurya@gmail.com>
2020-02-05 19:41:45 +0530

    changes on graph for stepper (carousel) effect

--- src/components/Dashboard/Daily.js
+++ src/components/Dashboard/Daily.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import Chart from "react-google-charts";
+import {useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 const Daily = () => {
+   const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = 52;
+  const theme = useTheme();
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
     return ( 
+      <div>
     <Chart
         width={'600px'}
         height={'400px'}
@@ -35,7 +51,27 @@ const Daily = () => {
         },
         }}
         rootProps={{ 'data-testid': '3' }}
-      />);
+      />
+       <MobileStepper
+        steps={maxSteps}
+        position="static"
+        variant="text"
+        activeStep={activeStep}
+        nextButton={
+          <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+            Next
+            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            Back
+          </Button>
+        }
+      />
+      </div>
+      );
 }
  
 export default Daily;
--- src/components/Dashboard/Monthly.js
+++ src/components/Dashboard/Monthly.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import Chart from "react-google-charts";
+import {useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 const Monthly = () => {
-    return ( <Chart
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = 12;
+  const theme = useTheme();
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
+    return ( 
+      <div>
+        <Chart
         width={'600px'}
         height={'400px'}
         chartType="LineChart"
@@ -54,7 +71,28 @@ const Monthly = () => {
           },
           }}
         rootProps={{ 'data-testid': '3' }}
-      />);
+        
+      />
+        <MobileStepper
+        steps={maxSteps}
+        position="static"
+        variant="text"
+        activeStep={activeStep}
+        nextButton={
+          <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
+            Next
+            {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+            Back
+          </Button>
+        }
+      />
+      </div>
+      );
 }
  
 export default Monthly;
