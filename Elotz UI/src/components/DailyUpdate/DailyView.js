6494db7ba3c7db7a306e7f873fbd41d06720130c
Sathish Kumar <30808898+raasalgul@users.noreply.github.com>
2020-03-13 12:15:32 +0530

    Update DailyView.js

--- Elotz UI/src/components/DailyUpdate/DailyView.js
+++ Elotz UI/src/components/DailyUpdate/DailyView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useEffect,useState} from "react"
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -9,7 +10,7 @@ import Grid from '@material-ui/core/Grid';
 const useStyles = makeStyles({
   root: {
     position: 'relative',
-    top:'250px'
+    top:'200px'
   },
   root_cardContent:{
     maxHeight: 275,
@@ -17,56 +18,117 @@ const useStyles = makeStyles({
   },
   card_root: {
     minWidth: 275,
-    maxWidth: 'fit-content'
+    maxWidth: 500,
+    minHeight:180,
+    maxHeight: 180,
+    backgroundColor: "#79B473",
+    color:"#ffffff"
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+  topic_style: {
+    color: "#70A37F",
+    fontSize: 32,
+    fontFamily:"Andale Mono",
+    padding:20
   },
-  title: {
-    fontSize: 14,
+  task_style: {
+    fontSize: 24
   },
-  pos: {
-    marginBottom: 12,
+  next_button:{
+     backgroundColor: "#41658A",
+     color:"#ffffff",
+     float:"left",
+     margin:10,  
+     '&:hover': {
+      backgroundColor:'#414073',
+      color: '#FFF'
+  }
   },
+  prev_button:{
+    backgroundColor: "#41658A",
+    color:"#ffffff",
+    float:"right",
+    margin:10,
+    '&:hover': {
+      backgroundColor:'#41658A',
+      color: '#FFF'
+  }
+ }
 });
-
+let topicIndex=0;
+const test={
+  'java':['Oops','Collections','Variables','Loops','Date','String'],
+  'react':['Hooks','Functional components','Class components'],
+  'mongo db':['DML','DDL','transations']
+}
 export default function DailyView() {
   const classes = useStyles();
-const response=['java','react','mongo db','Jax-RS','hooks','react','mongo db','Jax-RS','hooks','react','mongo db','Jax-RS','hooks','react','mongo db','Jax-RS','hooks'];
-  return (
-    <Card className={classes.root}>
-      <CardContent className={classes.root_cardContent}>
-          <Grid container spacing={2}>
-          {response.map(value=>{
-              return(
-               <Grid item xs={3}>
-               <Card className={classes.card_root}>
-               <CardContent>
-                 <Typography className={classes.title} color="textSecondary" gutterBottom>
-                 Topic {value}
-                 </Typography>
-                 <Typography variant="h5" component="h2">
-                 World of {value}
-                 </Typography>
-                 <Typography className={classes.pos} color="textSecondary">
-                   Task
-                 </Typography>
-                 <Typography variant="body2" component="p">
-                   Description
-                   <br />
-                   {'"author"'}
-                 </Typography>
-               </CardContent>
-             </Card>
-               </Grid>);
-          })}
-      </Grid>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+// const response=['java','react','mongo db','Jax-RS','hooks','react','mongo db','Jax-RS','hooks','react','mongo db','Jax-RS','hooks','react','mongo db','Jax-RS','hooks'];
+
+const [topic,setTopic]=useState('');
+const [tasks,setTasks]=useState([]);
+useEffect(()=>{
+  var name=Object.keys(test)[0];
+  console.log(test[name]);
+  setTopic(name.toUpperCase());
+  setTasks(test[name]);
+  },[]);
+  function handleNext() {
+    topicIndex<Object.keys(test).length-1?topicIndex++:topicIndex=0;
+    // console.log(`${topicIndex} Length ${Object.keys(test).length}`);
+    var name=Object.keys(test)[topicIndex];
+    setTopic(name.toUpperCase());
+    setTasks(test[name]);
+   
+  }
+  function handlePrevious() {
+    topicIndex>0?--topicIndex:topicIndex=Object.keys(test).length-1;
+    // console.log(`${topicIndex} Length ${Object.length}`);
+    var name=Object.keys(test)[topicIndex];
+    setTopic(name.toUpperCase());
+    setTasks(test[name]);
+    //console.log(`Index ${name.toUpperCase()}`);
+  }
+  // console.dir(handleNext);
+return (
+      <div className={classes.root}>
+          <Typography variant="h5" component="h2" className={classes.topic_style}>
+                       {topic}
+                       </Typography>
+            <Card>
+            <CardContent className={classes.root_cardContent}>
+                <Grid container spacing={2}>
+                {
+                tasks.map((value,index)=>{
+                    return(
+                     <Grid key={index} item xs={3}>
+                     <Card className={classes.card_root}>
+                     <CardContent>
+                       <Typography className={classes.task_style} gutterBottom>
+                       Topic {value}
+                       </Typography>
+                       <Typography variant="body2" component="p">
+                         Description
+                         <br />
+                         {'"author"'}
+                       </Typography>
+                     </CardContent>
+                   </Card>
+                     </Grid>);
+                })}
+            </Grid>
+            </CardContent>
+            <CardActions>
+            <Grid container>
+            <Grid item xs={6}>
+              <Button size="small" variant="contained" color="default" className={classes.next_button} onClick={handlePrevious}>previous</Button>
+              </Grid>
+              <Grid item xs={6}>
+              <Button size="small" variant="contained" color="default" className={classes.prev_button} onClick={handleNext}>Next</Button>
+           </Grid>
+           </Grid>
+            </CardActions>
+          </Card>
+   </div>
   );
+  
 }
